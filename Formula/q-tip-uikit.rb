3bd773c9d2f3dd978defc4cbdbdd5e706394300a
Ayaan Banday <nebulaos.com@gmail.com>
2026-09-10 19:15:32 +0200

    update

--- Formula/q-tip-uikit.rb
+++ Formula/q-tip-uikit.rb
@@ -1,9 +1,9 @@
 class QTipUikit < Formula
   desc "A mod for Q-Tip that provides UI"
   homepage "https://github.com/ProPythonCoderAya/homebrew-q-tip-uikit"
-  url "NaN"
+  url "https://github.com/ProPythonCoderAya/homebrew-q-tip-uikit/archive/refs/tags/v0.0.0.tar.gz"
   head "https://github.com/ProPythonCoderAya/homebrew-q-tip-uikit.git", branch: "main"
-  sha256 "NaN"
+  sha256 "7fd16478f499677b58c075e4f7cfbf157dfc577defbd577fad8894e7f22ee5e7"
   license "MIT"
 
   depends_on "cmake"
